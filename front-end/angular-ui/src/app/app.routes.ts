dd55cb419a4d97623646586fee53656723e8d981
Kyler Johnson <kylerjohnsondev@gmail.com>
2025-08-03 13:15:13 -0400

    fix todo action dropdown, add header

--- front-end/angular-ui/src/app/app.routes.ts
+++ front-end/angular-ui/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import TodosPage from './todos/todos-page';
+import DefaultLayout from './layouts/default-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'register',
+    loadComponent: () => import('./auth/register-page')
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./auth/login-page')
+  },
+  {
+    path: '',
+    component: DefaultLayout,
+    children: [
+      { path: 'todos', component: TodosPage },
+      { path: 'account', loadComponent: () => import('./pages/account-page') },
+      { path: '', redirectTo: 'todos', pathMatch: 'full' },
+    ]
+  },
+];
